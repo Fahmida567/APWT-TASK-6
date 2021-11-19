430a442e7990239b5999248be83df0d11b560cfb
Turin <you@example.com>
2021-11-19 20:30:05 +0600

    Task complete

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+       <center> <h4>copyright © 2021 Student App</h4></center>
+      </div>
+    );
+};
+
+export default Footer;
---
+++ src/Components/StudentData.js
@@ -0,0 +1,57 @@
+const StudentData =[
+    {
+        id: 1,
+        name: "Fahmida Rashid",
+        studentId: "19-40040-1",
+        batch: "19-1",
+        cgpa: "3.78",
+        address: "Rajshahi",
+        department: "CSE"
+    },
+    {
+        id: 2,
+        name: "Srijoni",
+        studentId: "19-40030-1",
+        batch: "19-1",
+        cgpa: "3.5",
+        address: "Feni",
+        department: "CSE"
+    },
+    {
+        id: 3,
+        name: "Rakhi",
+        studentId: "19-40042-1",
+        batch: "19-1",
+        cgpa: "3.85",
+        address: "Dhaka",
+        department: "CSE"
+    },
+    {
+        id: 4,
+        name: "Musharrat",
+        studentId: "19-40077-1",
+        batch: "19-1",
+        cgpa: "3.8",
+        address: "Dhaka",
+        department: "CSE"
+    },
+    {
+        id: 5,
+        name: "Sristy",
+        studentId: "19-40084-1",
+        batch: "19-1",
+        cgpa: "3.5",
+        address: "Dhaka",
+        department: "CSE"
+    },
+    {
+        id: 6,
+        name: "Erjima",
+        studentId: "17-40037-1",
+        batch: "19-1",
+        cgpa: "3.5",
+        address: "Dhaka",
+        department: "CSE"
+    }
+]
+export default StudentData;
